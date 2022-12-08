2531c4ca8e174a537c1a586354d449a7cbee628b
protechoutsourcing <protechoutsourcing@gmail.com>
2022-12-08 09:11:59 +0700

    08-12-22 : Update

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const port = 3000; //กำหนด Port
 const path = require('path'); //ประกาศตัวแปรเรียกใช้ path
 
 app.use(morgan('combined')); //เรียกใช้ morgan
-app.use(express.static(path.join(__dirname,"/public/"))); //เรียกใช้ path
-app.get("/", (req,res) => { //request/response
-    res.send('NodeExpress101'); //ตอบกลับทาง browser
+app.use(express.static(path.join(__dirname, "/public/"))); //เรียกใช้ path
+
+app.get("/", (req, res) => { //request/response
+    res.send('NodeExpress101'); //แสดงผลที่ browser
 })
 
-app.listen(port,() => { //ตอบกลับที่พอร์ท
-    debug(chalk.yellow('Listening on port :'),chalk.greenBright(port)); //แสดง log,ใช้ chalk เปลี่ยนสีตัวหนังสือ
+app.listen(port, () => { //ตอบกลับที่พอร์ท
+    debug(chalk.yellow('Listening on port :'), chalk.greenBright(port)); //แสดง log ที่ terminal,ใช้ chalk เปลี่ยนสีตัวหนังสือ
 })
